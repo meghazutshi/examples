64ffbea8e6fc9c85c7e8ca4d9f796149d9a3ba77
meghaz <meghaz@192.168.0.122>
2020-04-03 17:51:28 -0700

    committing changes to duplicate search

--- codingSample/src/main/java/io/megha/codingQuestions/DuplicateCharacterSearch.java
+++ codingSample/src/main/java/io/megha/codingQuestions/DuplicateCharacterSearch.java
@@ -1,5 +1,6 @@
 package io.megha.codingQuestions;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,21 +11,51 @@ import java.util.Map;
  */
 public class DuplicateCharacterSearch {
 
+
 	public static void main(String[] args) {
-		String inputString="abaabaxaz";
+		String inputString="axaaAbs";
 		System.out.println("inputString: " + inputString);
+		System.out.println("Duplicate Characters Present in the String: "+ duplicatesPresent(inputString));
+		System.out.println("Complete list of duplicates: ");
+		fullListOfDuplicates(inputString);
+	}
+	private static void fullListOfDuplicates(String inputString) {
+		if(null!=inputString) {
 		Map<String, Integer> duplicateSotter = new HashMap<String, Integer>();
 		for(int i=0; i<inputString.length();i++) {
-			Integer count = duplicateSotter.get(String.valueOf(inputString.charAt(i)));
-			if(null!=count) {
-				duplicateSotter.put(String.valueOf(inputString.charAt(i)), count+1);
+			Integer count = duplicateSotter.containsKey(String.valueOf(inputString.charAt(i)))? duplicateSotter.
+					get(String.valueOf(inputString.charAt(i))):0;
+			duplicateSotter.put(String.valueOf(inputString.charAt(i)), count+1);
+			
+		}
+			duplicateSotter.forEach((x,y)->System.out.println("Character: "+x+" Count: "+y));
+		}
+	}
+	
+	private static boolean duplicatesPresent(String inputString) {
+		if(null!= inputString) {
+		// Sorting to improve the efficiency
+		inputString = sortString(inputString);
+		Map<String, Integer> duplicateChecker = new HashMap<String, Integer>();
+		for(int i=0; i<inputString.length();i++) {
+			if(duplicateChecker.containsKey(String.valueOf(inputString.charAt(i)))) {
+				return true;
 			}
 			else {
-				duplicateSotter.put(String.valueOf(inputString.charAt(i)), 1);
+				duplicateChecker.put(String.valueOf(inputString.charAt(i)), 1);
 			}
 			
 		}
-		duplicateSotter.forEach((x,y)->System.out.println("Character: "+x+" Count: "+y));
+		}
+		return false;
 	}
+	private static String sortString(String inputString) {
+		char tempSorted[] = inputString.toCharArray();
+		Arrays.sort(tempSorted);
+		inputString=String.valueOf(tempSorted);
+		System.out.println("Sorted Input: "+ inputString);
+		return inputString;
+	}
+
 
 }
